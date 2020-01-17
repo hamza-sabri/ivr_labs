c8b6a1cb86e358f3194c903de571c0bcf54a5a49
Omar Sabri <oz9.sa7@gmail.com>
2020-01-17 16:32:56 +0200

    Adding support for iOS devices

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\oz9sa\Documents\GitHub\ivr_labs"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/omzer/Desktop/3mr/Programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/omzer/Desktop/ivr_labs"
+export "FLUTTER_TARGET=/Users/omzer/Desktop/ivr_labs/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/omzer/Desktop/3mr/Programs/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
